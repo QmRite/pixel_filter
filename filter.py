5886c03ca86d124d10a4f814de5a5166ee2e62cc
QmRite <lopesmigel9@gmail.com>
2021-11-22 19:51:57 +0500

    add more inputs

--- filter.py
+++ filter.py
@@ -3,27 +3,26 @@ import numpy as np
 
 
 def count_elem_color():
-    color_sum = np.sum(pixels[i: i + mosaic_size, j: j + mosaic_size])
-    return int(color_sum // (mosaic_size * mosaic_size))
+    color_sum = np.sum(pixels[i: i + block_size, j: j + block_size])
+    return int(color_sum // (block_size * block_size))
 
 
 def generate_black_and_white_elem():
     medium_color = int(elem_color // grayscale) * grayscale / 3
-    pixels[i: i + mosaic_size, j: j + mosaic_size] = medium_color
+    pixels[i: i + block_size, j: j + block_size] = medium_color
 
 
-print('Введите названия исходного изображения и результата через пробел')
-inp_name, out_name = input().split()
-img = Image.open(inp_name)
+img = Image.open(input('Введите названия исходного изображения: '))
+block_size = int(input("Введите размер блока: "))
+grayscale = 255 // int(input("Введите количество градаций серого: "))
 pixels = np.array(img)
 height = len(pixels)
 width = len(pixels[1])
-mosaic_size = 10
-grayscale = 50
 
-for i in range(0, height, mosaic_size):
-    for j in range(0, width, mosaic_size):
+for i in range(0, height, block_size):
+    for j in range(0, width, block_size):
         elem_color = count_elem_color()
         generate_black_and_white_elem()
 res = Image.fromarray(pixels)
-res.save(out_name)
+
+res.save(input("Введите название обработанного изображения: "))
