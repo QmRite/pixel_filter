d5623e85e187f0b14f9f318e7888f75209ea0392
QmRite <82218195+QmRite@users.noreply.github.com>
2021-11-27 20:42:40 +0500

    PEP8

--- filter.py
+++ filter.py
@@ -2,12 +2,32 @@ from PIL import Image
 import numpy as np
 
 
-def count_elem_color():
+def count_mean_color(block_size):
+    """
+     Вычисляет средный оттенок блока заданного размера
+     :param block_size: размер блока
+     :return: cредний оттенок
+
+    >>> count_mean_color(1)
+    300
+    >>> count_mean_color(10)
+    300
+    >>> count_mean_color(15)
+    133
+    >>> count_mean_color(-1)
+    0
+    """
     color_sum = np.sum(pixels[i: i + block_size, j: j + block_size])
     return int(color_sum // (block_size * block_size))
 
 
-def generate_black_and_white_elem():
+def generate_black_and_white_elem(block_size, elem_color, grayscale):
+    """
+     Перекрашивает блок в черно-белые оттенки
+     :param block_size: размер блока
+     :param elem_color: средний цвет блока
+     :param grayscale: кол-во градаций серого
+    """
     medium_color = int(elem_color // grayscale) * grayscale / 3
     pixels[i: i + block_size, j: j + block_size] = medium_color
 
